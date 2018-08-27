582bfb889fc5174e37153ff526c8dbd03033d4a7
rajeev <rajeev29five@gmail.com>
2018-08-28 01:14:19 +0530

    Update

--- app/src/main/java/com/example/rajeev/shareride/MainActivity.java
+++ app/src/main/java/com/example/rajeev/shareride/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.rajeev.shareride;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,11 +18,8 @@ import com.example.rajeev.shareride.fragment.ActiveRides;
 import com.example.rajeev.shareride.fragment.PreviousRides;
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
- * Created by rajeev on 4/3/18.
- */
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends BasicActivity {
     private static final String TAG = "MainActivity";
 
     private FragmentPagerAdapter mPagerAdapter;
@@ -29,46 +29,43 @@ public class MainActivity extends BasicActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             private final Fragment[] mFragments = new Fragment[]{
                     new ActiveRides(),
                     new PreviousRides(),
             };
             private final String[] mFragmentNames = new String[]{
-                    getString(R.string.heading_recent),
-                    getString(R.string.heading_my_posts),
+                    "Active Rides",
+                    "Previous Rides",
             };
-
             @Override
             public Fragment getItem(int position) {
                 return mFragments[position];
             }
-
             @Override
             public int getCount() {
                 return mFragments.length;
             }
-
             @Override
             public CharSequence getPageTitle(int position) {
                 return mFragmentNames[position];
             }
         };
+        // Set up the ViewPager with the sections adapter.
         mViewPager = findViewById(R.id.container);
         mViewPager.setAdapter(mPagerAdapter);
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        findViewById(R.id.fab_new_post).setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_new_post);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, NewPostActivity.class));
+
             }
         });
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -87,4 +84,5 @@ public class MainActivity extends BasicActivity {
             return super.onOptionsItemSelected(item);
         }
     }
+
 }
