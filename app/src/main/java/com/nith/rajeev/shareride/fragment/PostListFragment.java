ee9d9c15db847756e2dc978cd2815a0f08c995c8
rajeev <rajeev29five@gmail.com>
2019-02-20 22:49:03 +0530

    list fragment

--- app/src/main/java/com/nith/rajeev/shareride/fragment/PostListFragment.java
+++ app/src/main/java/com/nith/rajeev/shareride/fragment/PostListFragment.java
@@ -1,14 +1,23 @@
 package com.nith.rajeev.shareride.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
 import com.nith.rajeev.shareride.PostDetailActivity;
 import com.nith.rajeev.shareride.R;
 import com.nith.rajeev.shareride.models.Post;
@@ -87,16 +96,71 @@ public abstract class PostListFragment extends Fragment {
                     }
                 });
 
-                viewHolder.bindToPost(model); /**, new View.OnClickListener() {
+
+                if (model.uid.equals(getUid())) {
+                    viewHolder.deleteButton.setEnabled(true);
+                    viewHolder.deleteButton.setVisibility(View.VISIBLE);
+                }
+                else {
+                    viewHolder.deleteButton.setEnabled(false);
+                    viewHolder.deleteButton.setVisibility(View.GONE);
+                }
+
+                if (getTag().endsWith("1")) {
+                    viewHolder.deleteButton.setEnabled(false);
+                    viewHolder.deleteButton.setVisibility(View.GONE);
+                }
+
+
+                viewHolder.bindToPost(model, new View.OnClickListener() {
                     @Override
-                    public void onClick(View starView) {
-                        DatabaseReference globalPostRef = mDatabase.child("posts").child(postRef.getKey());
-                        DatabaseReference userPostRef = mDatabase.child("user-posts").child(model.uid).child(postRef.getKey());
+                    public void onClick(View deleteButton) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                        builder.setMessage("Are you sure?");
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                DatabaseReference globalPostRef = mDatabase.child("posts").child(postRef.getKey());
+                                onDeleteClicked(globalPostRef);
+                                Toast.makeText(getContext(), "Post Deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
                     }
-                });**/
+                });
             }
         };
         mRecycler.setAdapter(mAdapter);
+    }
+
+    private void onDeleteClicked(DatabaseReference postRef) {
+        postRef.runTransaction(new Transaction.Handler() {
+            @NonNull
+            @Override
+            public Transaction.Result doTransaction(@NonNull MutableData mutableData) {
+                Post p = mutableData.getValue(Post.class);
+                if (p == null) {
+                    return Transaction.success(mutableData);
+                }
+
+                p = null;
+                mutableData.setValue(p);
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(@Nullable DatabaseError databaseError, boolean b, @Nullable DataSnapshot dataSnapshot) {
+
+            }
+        });
     }
 
 
