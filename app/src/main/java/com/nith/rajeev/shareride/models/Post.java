db5382b7f738ad92cd9df64e72c45567f9e0dd18
rajeev <rajeev29five@gmail.com>
2019-02-23 15:37:47 +0530

    Updated

--- app/src/main/java/com/nith/rajeev/shareride/models/Post.java
+++ app/src/main/java/com/nith/rajeev/shareride/models/Post.java
@@ -13,13 +13,9 @@ public class Post{
         public String uid;
         public String author;
         public String title;
-//        public String source;
-//        public String destination;
         public String dateOfJourney;
         public String timeOfJourney;
         public String body;
-//        public int starCount = 0;
-//        public Map<String, Boolean> stars = new HashMap<>();
 
         public Post() {
             // Default constructor required for calls to DataSnapshot.getValue(Post.class)
@@ -29,29 +25,21 @@ public class Post{
             this.uid = uid;
             this.author = author;
             this.title = title;
-//            this.source = source;
-//            this.destination = destination;
             this.dateOfJourney = dateOfJourney;
             this.timeOfJourney = timeOfJourney;
             this.body = body;
         }
 
-        // [START post_to_map]
         @Exclude
         public Map<String, Object> toMap() {
             HashMap<String, Object> result = new HashMap<>();
             result.put("uid", uid);
             result.put("author", author);
             result.put("title", title);
-//            result.put("source", source);
-//            result.put("destination", destination);
             result.put("dateOfJourney", dateOfJourney);
             result.put("timeOfJourney", timeOfJourney);
             result.put("body", body);
-//            result.put("starCount", starCount);
-//            result.put("stars", stars);
 
             return result;
         }
-        // [END post_to_map]
 }
