953ca3fc455b8890db5441ec8087b3d7db5279b1
rajeev <rajeev29five@gmail.com>
2019-02-23 15:33:25 +0530

    Updated

--- app/src/main/java/com/nith/rajeev/shareride/AppInfo.java
+++ app/src/main/java/com/nith/rajeev/shareride/AppInfo.java
@@ -7,15 +7,9 @@ import android.widget.TextView;
 public class AppInfo extends AppCompatActivity {
 
 
-    private TextView firstContributor;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-//
-//        firstContributor = findViewById(R.id.first_contributor);
-//        firstContributor.setMovementMethod(LinkMovementMethod.getInstance());
-//        firstContributor.setTextColor(getResources().getColor(R.color.link_blue));
     }
 }
